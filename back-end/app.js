1b5a40bf54641c4b28685200eb3597d60055983a
jede <jedebrahma@pursuit.org>
2022-08-28 00:41:27 -0400

    added working ReadMe skeletons on back-end

---
+++ back-end/app.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+
+const express = require("express");
+const cors = require("cors");
+// CONFIGURATION
+const app = express();
+
+//CONTROLLERS
+// const entriesController = require("./controllers/entriesController.js");
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// app.use("/entries", entriesController);
+
+// ROUTES
+//home
+app.get("/", (req, res) => {
+    res.send("Welcome to Soul Sync !")
+});
+//404
+app.get("*", (req, res) => {
+    res.status(404).send("oops - page not found!")
+});
+
+// EXPORT
+module.exports = app;
